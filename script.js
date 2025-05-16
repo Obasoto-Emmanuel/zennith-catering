80ecb9e31d0d35f64785b0f168430702ef07ec16
Obasoto-Emmanuel <obasotoemmanuel@gmail.com>
2025-05-16 19:08:02 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Navigation toggle
+const toggle = document.getElementById('toggle');
+const navLinks = document.getElementById('navLinks');
+
+toggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+// Hero image slideshow
+const hero = document.querySelector('.hero');
+const images = [
+    'CSS/IMAGES/Wedding Decor 1.jpg',
+  'CSS/IMAGES/Fish.jpg',
+  'CSS/IMAGES/Strange Rice.jpg',
+];
+
+let index = 0;
+
+images.forEach(src => {
+  const img = new Image();
+  img.src = src;
+});
+
+
+function changeHeroBackground() {
+  hero.style.backgroundImage = `url('${images[index]}')`;
+  index = (index + 1) % images.length;
+}
+
+changeHeroBackground(); // Set first image
+setInterval(changeHeroBackground, 5000); // Change every 5 seconds
